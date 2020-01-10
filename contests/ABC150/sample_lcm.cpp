363338cb7edcaec868df9dc414adfcecd2c3e80d
Terajima <ryothailand98@gmail.com>
2020-01-11 00:17:59 +0900

    sample function to calculate gcd and lcm

---
+++ contests/ABC150/sample_lcm.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+
+typedef long long ll;
+
+// ユークリッドの互除法
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a; }
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b; }
+
+int main(){
+  // 処理
+  return 0;
+}
