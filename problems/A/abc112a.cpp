33dd173fc667d670aeb5af99cbdf2c0db49feec1
Ryo Terajima <ryothailand98@gmail.com>
2020-03-22 22:50:59 +0900

    practice questions and ABC159

---
+++ problems/A/abc112a.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+using ll = long long;
+using VL = vector<ll>;
+using VVL = vector<vector<ll>>;
+using P = pair<ll, ll>;
+
+// chmin, chmax関数
+template<typename T, typename U, typename Comp=less<>>
+bool chmax(T& xmax, const U& x, Comp comp={}) {
+    if(comp(xmax, x)) {
+        xmax = x;
+        return true;
+    }
+    return false;
+}
+template<typename T, typename U, typename Comp=less<>>
+bool chmin(T& xmin, const U& x, Comp comp={}) {
+    if(comp(x, xmin)) {
+        xmin = x;
+        return true;
+    }
+    return false;
+}
+
+//---------------------------
+
+int main(){
+    int n;
+    cin >> n;
+    if(n==1) cout << "Hello World" << endl;
+    else{
+        int a,b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
