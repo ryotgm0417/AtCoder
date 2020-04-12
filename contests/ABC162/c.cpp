2dbb47dfba8f75d205130928bdc320d762077e00
Ryo (lab pc) <ryothailand98@gmail.com>
2020-04-12 22:43:10 +0900

    ABC162

---
+++ contests/ABC162/c.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for (int i=0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+using ll = long long;
+using VL = vector<ll>;
+using VVL = vector<vector<ll>>;
+using P = pair<ll, ll>;
+
+// chmin, chmax関数
+template<typename T, typename U, typename Comp=less<>>
+bool chmax(T& xmax, const U& x, Comp comp={}) {
+    if(comp(xmax, x)) {
+        xmax = x;
+        return true;
+    }
+    return false;
+}
+template<typename T, typename U, typename Comp=less<>>
+bool chmin(T& xmin, const U& x, Comp comp={}) {
+    if(comp(x, xmin)) {
+        xmin = x;
+        return true;
+    }
+    return false;
+}
+
+// Yes-Noを出力する問題で楽をする
+void Ans(bool f){
+    if(f) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
+
+// ユークリッドの互除法
+ll gcd(ll a, ll b) { return b?gcd(b,a%b):a; }
+ll lcm(ll a, ll b) { return a/gcd(a,b)*b; }
+
+//---------------------------
+
+int main(){
+    ll k; cin >> k;
+    ll sum = 0;
+
+    rep2(a,1,k+1){
+        rep2(b,1,k+1){
+            ll x = gcd(a,b);
+            rep2(c,1,k+1){
+                ll y = gcd(x,c);
+                sum += y;
+            }
+        }
+    }
+
+    cout << sum << endl;
+    return 0;
+}
